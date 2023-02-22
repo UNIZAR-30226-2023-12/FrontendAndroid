630da852878f1c14e9d49e94e8cc7b4626bb0986
Juan Pellicer <jpellicerbarco@gmail.com>
2023-02-22 12:33:15 +0100

    Un primer modelo de la interfaz

---
+++ app/src/main/java/eina/unizar/melodiaapp/LogIn.java
@@ -0,0 +1,26 @@
+package eina.unizar.melodiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        TextView acces = findViewById(R.id.bAcceder);
+        acces.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Menu.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/eina/unizar/melodiaapp/MainActivity.java
@@ -0,0 +1,35 @@
+package eina.unizar.melodiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView bLogin = findViewById(R.id.bLogin);
+        bLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView bRegister = findViewById(R.id.bRegister);
+        bRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
