88379d3ab6901abea7ed81557438eefed19ee828
mariooc04 <mariooc04@gmail.com>
2023-03-15 12:42:14 +0100

    Añadidos módulos Nuevos módulos: 	- AudioConversions: que permite realizar todas las conversiones que he considerado que se van a necesitar a la hora de tratar archivos de audio 	- AudioPlayer: contiene métodos básicos para la reproducción de audio

---
+++ app/src/main/java/eina/unizar/melodiaapp/Modules/AudioConversions.java
@@ -0,0 +1,52 @@
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.Base64;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+
+import java.util.Base64;
+
+/*
+ * Módulo que contiene una clase con los métodos necesarios para convertir strings en arrays de bytes
+ * y después en archivos de audio y viceversa.
+ */
+
+public class AudioConversions {
+    private string binaryString;
+    private byte[] byteArray;
+    private File inputFile;
+
+    // Constructor, si se quiere conseguir un archivo de audio a partir de un string en file pasar null
+    // Si se quiere conseguir un string en base64 a partir de un archivo de audio pasar null en el string
+    public JSONConversions(string binaryString, File file) {
+        this.binaryString = binaryString;
+        this.inputFile = file;
+    }
+
+    // Método para convertir un string en un array de bytes
+    public void convertStringToByteArray() {
+        this.byteArray = Base64.getDecoder().decode(this.binaryString);
+    }
+
+    // Método para convertir un array de bytes en un archivo de audio
+    public File convertByteArrayToFile(String fileName) {
+        File outputFile = new File(fileName);
+
+        AudioInputStream audioInputStream = new AudioInputStream(new ByteArrayInputStream(byteArray), new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false), byteArray.length / 4);
+
+        AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, outputFile);
+
+        return outputFile;
+    }
+
+    // Método para convertir un archivo de audio en un string en base64
+    public String convertFileToString() {
+        byte[] fileContent = Files.readAllBytes(this.inputFile.toPath());
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+        return encodedString;
+    }
+}
---
+++ app/src/main/java/eina/unizar/melodiaapp/Modules/AudioPlayer.java
@@ -0,0 +1,75 @@
+import android.media.MediaPlayer;
+
+
+public class AudioPlayer {
+    private boolean paused = false;
+    private String path;
+    private MediaPlayer mediaPlayer;
+
+
+    /*
+     * Constructor de la clase, recibe como parámetro la ruta del archivo de audio que se quiere reproducir.
+     */
+    public AudioPlayer(String path) {
+        this.path = path;
+        this.mediaPlayer = new MediaPlayer();
+    }
+
+    /*
+     * Método para reproducir el audio, si ya se estaba reproduciendo se para y se reproduce el nuevo.
+     */
+    public void play(String path) {
+        try {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer.reset();
+                mediaPlayer = new MediaPlayer(path);
+            }
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * Método para pausar el audio, si no se estaba reproduciendo no hace nada.
+     */
+    public void pause() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            paused = true;
+        }
+    }
+
+    /*
+     * Método para reanudar el audio, si no se estaba reproduciendo no hace nada.
+     */
+    public void resume() {
+        if (paused) {
+            mediaPlayer.start();
+            paused = false;
+        }
+    }
+
+    /*
+     * Método para parar el audio, si no se estaba reproduciendo no hace nada.
+     */
+    public void stop() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer.reset();
+        }
+    }
+
+    /*
+     * Método para saber si se está reproduciendo o no algún audio.
+     */
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+}
