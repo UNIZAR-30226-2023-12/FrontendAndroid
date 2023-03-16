0586fededce12dd982c756bc1e967ff76fe63dfe
Juan Pellicer <jpellicerbarco@gmail.com>
2023-03-16 12:46:28 +0100

    Añadida la pantalla para añadir carpetas y la pantalla de selección de listas de reproducción

--- app/src/main/java/eina/unizar/melodiaapp/Carpeta.java
+++ app/src/main/java/eina/unizar/melodiaapp/Carpeta.java
@@ -2,7 +2,10 @@ package eina.unizar.melodiaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Carpeta extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class Carpeta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carpeta);
+
+        TextView bCarpetaEjemplo = findViewById(R.id.carpetaEjemplo);
+        bCarpetaEjemplo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), playlist.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView bAddCarpeta = findViewById(R.id.bMasCarpetas);
+        bAddCarpeta.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateCarpeta.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/eina/unizar/melodiaapp/CreateCarpeta.java
@@ -0,0 +1,14 @@
+package eina.unizar.melodiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateCarpeta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_carpeta);
+    }
+}
