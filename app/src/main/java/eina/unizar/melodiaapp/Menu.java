95a305ccab2858af760512552433752260ef3126
Ismael-V <ismaelpsabi@gmail.com>
2023-03-17 14:20:31 +0100

    Enmendado de errores en activity player y terminado el boton de play pause, añadido boton stop, arreglado boton de favoritos (pendiente de redibujado)

--- app/src/main/java/eina/unizar/melodiaapp/Menu.java
+++ app/src/main/java/eina/unizar/melodiaapp/Menu.java
@@ -2,7 +2,10 @@ package eina.unizar.melodiaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         getSupportActionBar().hide();
+
+        TextView boton_aleatoria = findViewById(R.id.bRandom);
+        boton_aleatoria.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Player.class);
+                startActivity(intent);
+            }
+        });
     }
 }
