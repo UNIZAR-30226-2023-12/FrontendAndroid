16f1752bf346663f9e708994d644838b4320a281
Juan Pellicer <jpellicerbarco@gmail.com>
2023-03-19 17:40:01 +0100

    Añadida segunda pantalla de recuperación de contraseñas con comprobación de que ambas sean iguales

--- app/src/main/java/eina/unizar/melodiaapp/PasswordRecover.java
+++ app/src/main/java/eina/unizar/melodiaapp/PasswordRecover.java
@@ -2,7 +2,10 @@ package eina.unizar.melodiaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PasswordRecover extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class PasswordRecover extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_recover);
+
+        TextView bCode = findViewById(R.id.bSendCodeConfirm);
+        bCode.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PasswordRecoverCode.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/eina/unizar/melodiaapp/PasswordRecoverCode.java
@@ -0,0 +1,44 @@
+package eina.unizar.melodiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PasswordRecoverCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_recover_code);
+
+        TextView bCheckPasswd = findViewById(R.id.bNewPasswordConfirm);
+        bCheckPasswd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                EditText password1 = findViewById(R.id.inNewPassword);
+                EditText password2 = findViewById(R.id.inNewPassword);
+
+                String pass1 = password1.getText().toString();
+                String pass2 = password2.getText().toString();
+
+                if (pass1.length() < 7 || pass2.length() < 7){
+                    Toast.makeText(getApplicationContext(), "Las contraseñas deben tener almenos 7 letras o números", Toast.LENGTH_SHORT).show();
+                }
+                else if(pass1.equals(pass2)){
+                    Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
