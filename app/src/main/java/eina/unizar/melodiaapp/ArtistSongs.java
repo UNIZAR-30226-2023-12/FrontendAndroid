2a672ddf124aaf1bf2e59fcf79f009b3a042f6bb
mariooc04 <mariooc04@gmail.com>
2023-05-01 17:44:48 +0200

    Añadida parte de la lógica para que el usuario artista pueda ver las canciones que ha ido subiendo

--- app/src/main/java/eina/unizar/melodiaapp/ArtistSongs.java
+++ app/src/main/java/eina/unizar/melodiaapp/ArtistSongs.java
@@ -12,19 +12,17 @@ import android.widget.ListView;
 import java.util.concurrent.ExecutionException;
 
 import eina.unizar.melodiaapp.Modules.MyTaskAskNameSongs;
-import eina.unizar.melodiaapp.Modules.MyTaskAskSongs;
+import eina.unizar.melodiaapp.Modules.MyTaskAskSongsArtist;
 
-public class playlist_favoritos extends AppCompatActivity {
-
+public class ArtistSongs extends AppCompatActivity {
     protected String[] doRequestAskSongs() throws ExecutionException, InterruptedException {
         // Obtengo usuario y contraseña
         SharedPreferences preferences = getSharedPreferences("credenciales", MODE_PRIVATE);
         String idUsuario = preferences.getString("idUsuario", "");
         String contrasenya = preferences.getString("contrasenya", "");
-        String idListaFavUser = preferences.getString("idListaFavoritos", "");
 
-        MyTaskAskSongs task = new MyTaskAskSongs();
-        String respuesta = task.execute(idUsuario, contrasenya, idListaFavUser).get();
+        MyTaskAskSongsArtist task = new MyTaskAskSongsArtist();
+        String respuesta = task.execute(idUsuario, contrasenya).get();
         String response[] = respuesta.split(",");
 
         if (response[0].equals("200")) {
@@ -56,54 +54,49 @@ public class playlist_favoritos extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_playlist_favoritos);
+        setContentView(R.layout.activity_artist_songs);
 
-        // Hago la request para obtener las canciones de una playlist
-        String idsSongs[] = new String[]{"Error"};
+        // Obtengo los ids de las canciones del artista
+        String[] idsCanciones = new String[]{"Error"};
         try {
-            idsSongs = doRequestAskSongs();
+            idsCanciones = doRequestAskSongs();
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
 
-        if (idsSongs[0].equals("200")) {
-            String nombresSongs[] = new String[idsSongs.length - 1];
-            for (int i = 1; i < idsSongs.length; i++) {
+        if (idsCanciones[0].equals("200")) {
+            // Obtengo los nombres de las canciones del artista
+            String[] nombresCanciones = new String[idsCanciones.length - 1];
+            for (int i = 1; i < idsCanciones.length; i++) {
                 try {
-                    nombresSongs[i - 1] = doRequestAskNameSongs(idsSongs[i]);
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
+                    nombresCanciones[i - 1] = doRequestAskNameSongs(idsCanciones[i]);
+                } catch (ExecutionException | InterruptedException e) {
                     e.printStackTrace();
                 }
             }
 
             // Muestro los nombres de las canciones en la interfaz
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.cancion_item, R.id.listTextView, nombresSongs);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.cancion_item, R.id.listTextView, nombresCanciones);
             ListView listView = findViewById(R.id.listSongsArtist);
             listView.setAdapter(adapter);
         }
 
-
-        // OnClick para el botón home
-        ImageView homeBTN = findViewById(R.id.menuIconAlistarepFav);
-        homeBTN.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), Menu.class);
-            startActivity(intent);
+        // OnClick botón home
+        ImageView home = findViewById(R.id.menuIconArtistSongs);
+        home.setOnClickListener(v -> {
+            Intent intent = new Intent(ArtistSongs.this, Menu.class);
         });
 
-        // OnClick para el botón de notificaciones
-        ImageView notifBTN = findViewById(R.id.bellIconAlistarepFav);
-        notifBTN.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), Notifications.class);
-            startActivity(intent);
+        // OnClick botón perfil
+        ImageView profile = findViewById(R.id.profileIconArtistSongs);
+        profile.setOnClickListener(v -> {
+            Intent intent = new Intent(ArtistSongs.this, Profile.class);
         });
 
-        // OnClick para el botón de perfil
-        ImageView profileBTN = findViewById(R.id.profileIconAlistarepFav);
-        profileBTN.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), Profile.class);
-            startActivity(intent);
+        // OnClick botón de notificaciones
+        ImageView notifications = findViewById(R.id.bellIconArtistSongs);
+        notifications.setOnClickListener(v -> {
+            Intent intent = new Intent(ArtistSongs.this, Notifications.class);
         });
     }
 }
--- app/src/main/java/eina/unizar/melodiaapp/Menu.java
+++ app/src/main/java/eina/unizar/melodiaapp/Menu.java
@@ -3,15 +3,37 @@ package eina.unizar.melodiaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.concurrent.ExecutionException;
+
+import eina.unizar.melodiaapp.Modules.MyTaskAskProfile;
+
 /**
  * Clase que codifica la actividad del menu principal de la aplicación
  */
 public class Menu extends AppCompatActivity {
+    protected String[] doRequestAskUser() throws ExecutionException, InterruptedException {
+        // Obtengo usuario y contraseña de shared preferences
+        SharedPreferences preferences = getSharedPreferences("credenciales", MODE_PRIVATE);
+        String idUsuario = preferences.getString("idUsuario", "");
+        String contrasenya = preferences.getString("contrasenya", "");
+
+        MyTaskAskProfile task = new MyTaskAskProfile();
+        String respuesta = task.execute(idUsuario, contrasenya).get();
+        String response[] = respuesta.split(",");
+
+        if (response[0].equals("200")) {
+            return response;
+        }
+        else {
+            return new String[]{"Error"};
+        }
+    }
 
     /**
      * Función invocada al crear la pantalla. Inicializa todos los elementos de la interfaz de usuario
@@ -27,6 +49,33 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         getSupportActionBar().hide();
+
+        // Obtengo el tipo de usuario
+        String[] response = new String[]{"Error"};
+        try {
+            response = doRequestAskUser();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Configuro el botón para que solo sea visible si el usuario es artista
+        TextView artistsSongsBTN = findViewById(R.id.ArtistSongs);
+        if (response[0].equals("200") && response[1].equals("artista")) {
+            artistsSongsBTN.setVisibility(View.VISIBLE);
+            // Añado el onClick también
+            artistsSongsBTN.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), ArtistSongs.class);
+                    startActivity(intent);
+                }
+            });
+        }
+        else {
+            artistsSongsBTN.setVisibility(View.GONE);
+        }
 
         TextView boton_aleatoria = findViewById(R.id.bRandom);
         boton_aleatoria.setOnClickListener(new View.OnClickListener(){
