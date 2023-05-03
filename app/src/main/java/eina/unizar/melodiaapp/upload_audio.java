c9363915e705e2ce2f32678a49652ab0cf81848a
mariooc04 <mariooc04@gmail.com>
2023-05-03 17:07:50 +0200

    Ya se obtiene correctamente la ruta absoluta y se solicitan los permisos necesarios

--- app/src/main/java/eina/unizar/melodiaapp/upload_audio.java
+++ app/src/main/java/eina/unizar/melodiaapp/upload_audio.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,6 +19,8 @@ import java.util.concurrent.ExecutionException;
 import eina.unizar.melodiaapp.Modules.MyTaskUploadAudio;
 
 public class upload_audio extends AppCompatActivity {
+
+    private String ruta = "Error";
     /**
      * Hace la petición para subir una canción
      * Y convierte la ruta obtenida en un string en base64
@@ -36,12 +43,16 @@ public class upload_audio extends AppCompatActivity {
         EditText calidad = findViewById(R.id.InCalidad);
 
         // Convierto el fichero en un string en base64
-        File file = new File(rutaAudio);
+        String[] absolutePath = ruta.split(":");
+        absolutePath[0] = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + absolutePath[1];
+        File file = new File(absolutePath[0]);
         FileInputStream fis = new FileInputStream(file);
         byte[] data = new byte[(int) file.length()];
         fis.read(data);
         fis.close();
         String str = new String(data, "UTF-8");
+        // convierto str a base64
+        str = android.util.Base64.encodeToString(data, android.util.Base64.DEFAULT);
 
 
         // Hago la petición para subir la canción
@@ -59,6 +70,12 @@ public class upload_audio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_audio);
 
+        Intent intent2 = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent2.setData(uri);
+        startActivity(intent2);
+
+
         // Onclick para el botón de seleccionar audio
         findViewById(R.id.SelectAudio).setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -68,10 +85,10 @@ public class upload_audio extends AppCompatActivity {
 
         // Onclick para el botón de subir canción
         findViewById(R.id.bSubir).setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(), this.ruta, Toast.LENGTH_LONG).show();
             String response = "Error";
             try {
-                String prueba = "hola";
-                response = doRequestUpload(prueba);
+                response = doRequestUpload(this.ruta);
             } catch (ExecutionException e) {
                 e.printStackTrace();
             } catch (InterruptedException e) {
@@ -97,8 +114,7 @@ public class upload_audio extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             String rutaAudio = data.getData().getPath();
-            Toast.makeText(getApplicationContext(), rutaAudio, Toast.LENGTH_LONG).show();
-            String nombre = rutaAudio.substring(rutaAudio.lastIndexOf("/") + 1);
+            this.ruta = rutaAudio;
         }
     }
 }
