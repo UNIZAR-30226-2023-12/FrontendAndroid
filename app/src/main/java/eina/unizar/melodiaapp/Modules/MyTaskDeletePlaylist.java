bc25ed3d5de0c9b78d3ab5bafdc5b75b3e4050a0
mariooc04 <mariooc04@gmail.com>
2023-05-06 12:15:29 +0200

    Implementada la asynctask para borrar playlists

--- app/src/main/java/eina/unizar/melodiaapp/Modules/MyTaskDeletePlaylist.java
+++ app/src/main/java/eina/unizar/melodiaapp/Modules/MyTaskDeletePlaylist.java
@@ -1,4 +1,51 @@
 package eina.unizar.melodiaapp.Modules;
 
-public class MyTaskDeletePlaylist {
+import android.os.AsyncTask;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MyTaskDeletePlaylist extends AsyncTask<String, Void, String> {
+    @Override
+    public String doInBackground(String... params) {
+        String idUsuario = params[0];
+        String contrasenya = params[1];
+        String idLista = params[2];
+        String result = "";
+
+        try {
+            URL url = new URL("http://10.0.2.2:8081/DeleteLista/");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            String jsonInputString = "{\"idUsr\": \"" + idUsuario + "\", \"contrasenya\": \"" + contrasenya + "\" , \"idLista\": \"" + idLista + "\"}";
+
+            try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+                wr.writeBytes(jsonInputString);
+            }
+
+            int response = conn.getResponseCode();
+
+            if (response == HttpURLConnection.HTTP_OK) {
+                return "200";
+            }
+            else {
+                return "Error";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "200";
+    }
 }
